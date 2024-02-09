d52d711d5c0cf581a8527d3dc78d9977a58d63b9
Catherine Stafford <catherinestafford@Catherines-MacBook-Air.local>
2024-02-09 15:24:19 -0500

    importing login page to new frontend folder

--- vacation-planner-pro/src/main/frontend/src/App.jsx
+++ vacation-planner-pro/src/main/frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="box">
+        <h1>Log In</h1>
+        <div id="info">
+          <label for="userName">User Name:</label>
+          <input type="text" id="userName"></input>
+          <label for="password">Password:</label>
+          <input type="text" id="password"></input>
+          <label for="token">Company ID:</label>
+          <input type="text" id="token"></input>
+          <button>Submit</button>
+        </div>
+        <img id="logo" src="assets/images/logo.png"></img>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
