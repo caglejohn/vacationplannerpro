3be50d0ca185db4f4f9799added51c48151b7c4c
Catherine Stafford <catstaffo@gmail.com>
2024-04-29 19:02:06 -0400

    Add header + navlinks to login/signup/about pages and standardize the style

--- vacation-planner-pro/src/main/frontend/src/pages/Root.jsx
+++ vacation-planner-pro/src/main/frontend/src/pages/Root.jsx
@@ -1,9 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Root() {
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const headerPaths = ['/login', '/signup', '/about'];
+
+  const showHeader = headerPaths.includes(pathname);
+
   return (
     <main>
+      {showHeader && <Header />}
       <Outlet />
     </main>
   );
